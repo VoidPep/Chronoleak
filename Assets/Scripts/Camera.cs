5daa7f4d803c6b9c76c5a0e8e6831843d17105da
voidpep <pedrohm1009@gmail.com>
2025-01-23 23:48:13 -0300

    Teste de camera

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -1,15 +1,22 @@
+using System;
 using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
+    [Header("Target Settings")]
     public Transform target;
-    public float smoothTime = 0.3F;
-    private Vector3 _velocity = Vector3.zero;
-    
-    void Update()
+    public Vector3 offset = new(0f, 2f, -5f);
+
+    [Header("Camera Settings")]
+    public float followSpeed = 5f;
+    public float rotationSpeed = 10f;
+
+    private void LateUpdate()
     {
-        var targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
-    
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _velocity, smoothTime);
+        var targetPosition = target.position + target.TransformDirection(offset);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+
+        var targetRotation = Quaternion.LookRotation(target.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
