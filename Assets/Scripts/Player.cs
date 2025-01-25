0b54b3cb405471d9fb6825b260c0f8daf78368bc
voidpep <pedrohm1009@gmail.com>
2025-01-25 02:22:26 -0300

    Rotação do personagem

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,39 +8,50 @@ public class Player : MonoBehaviour
     public float playerSpeed = 2.0f;
     public float jumpHeight = 1.0f;
     public float gravityValue = -9.81f;
+    public float rotationSpeed = 2.0f;
 
     private void Start()
     {
-        controller = gameObject.AddComponent<CharacterController>();
+        controller = gameObject.GetComponent<CharacterController>();
     }
 
     private void Update()
+    {
+        SetPlayerToGround();
+        Movement();
+        Jump();
+        
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    private void SetPlayerToGround()
     {
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
         }
+    }
 
-        Movement();
-
+    private void Jump()
+    {
         if (Input.GetButtonDown("Jump") && groundedPlayer)
-        {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
-        }
 
         playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
     }
 
     private void Movement()
     {
         var move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        move.Normalize();
+        
         controller.Move(move * (Time.deltaTime * playerSpeed));
 
-        if (move != Vector3.zero)
+        if (move == Vector3.zero)
             return;
         
-        gameObject.transform.forward = move;
+        var toRotation = Quaternion.LookRotation(move, Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
     }
 }
