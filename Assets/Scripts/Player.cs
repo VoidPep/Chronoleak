71475229dc44c81b82ef4bacb43873c9c6161a24
voidpep <pedrohm1009@gmail.com>
2025-01-26 17:08:48 -0300

    Inicio das animações

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
     public float playerSpeed = 6.0f;
     public float rotationSpeed = 700f;
 
+    [SerializeField] private bool groundedPlayer;
+
     private void Start()
     {
         controller = gameObject.GetComponent<CharacterController>();
@@ -14,21 +16,31 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
+        SetPlayerToGround();
         Movement();
-        
+
         controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    private void SetPlayerToGround()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
     }
 
     private void Movement()
     {
         var move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         move.Normalize();
-        
+
         controller.Move(move * (Time.deltaTime * playerSpeed));
 
         if (move == Vector3.zero)
             return;
-        
+
         var toRotation = Quaternion.LookRotation(move, Vector3.up);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
     }
